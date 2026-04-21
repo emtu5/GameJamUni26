ffc481dc85fdb46508c061879e404b08b2410270
EncrustedPhantom <wmulicz@outlook.com>
2026-04-21 14:18:24 +0300

    fixed pointing bug

---
+++ camera.gd
@@ -0,0 +1,6 @@
+extends Camera2D
+
+@onready var player = get_parent()
+
+func _process(delta: float) -> void:
+	pass
--- flashlight.gd
+++ flashlight.gd
@@ -2,12 +2,13 @@ extends Node2D
 
 var mouse_vector = Vector2.ZERO
 @onready var ray:RayCast2D = $RayCast2D
+@onready var player = get_parent().get_parent()
 
 func _ready():
-	var player = get_parent().get_parent()
+	
 	ray.add_exception(player)
 	
 func _process(delta: float) -> void:
-	mouse_vector = mouse_vector.lerp(get_viewport().get_mouse_position(), 0.2) # get mouse position + smooth
+	mouse_vector = mouse_vector.lerp(get_global_mouse_position(), 0.2) # get mouse position + smooth
 	look_at(mouse_vector) #rotate towards mouse position
 	
