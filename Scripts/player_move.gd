abcc758ca2b5ec7c777415c24f7ea5e6224f0ae8
emtu5 <115076076+emtu5@users.noreply.github.com>
2026-04-22 22:56:58 +0300

    internal sanity meter

--- Scripts/player_move.gd
+++ Scripts/player_move.gd
@@ -8,13 +8,16 @@ var move_dir = Vector2.ZERO
 @onready var interactray = $Pivot/RayCast2D
 var hasFirstKey:bool
 var hasSecondKey:bool
+var hasLightSource:bool = false
+var isAttacked: bool = false
 @onready var foldercount:int = 0
-var sanity:int
-
+var sanity:float
+const SANITY_INCREMENT: float = 0.1
 func _ready():
 	sanity = 100
 
 func _physics_process(delta: float) -> void:
+	process_sanity()
 	var input_dir = Input.get_vector("move_left","move_right","move_up","move_down") #get input vector
 	move_dir = move_dir.lerp(input_dir, 0.265) #lerp for smoothing
 	velocity = move_dir * speed # apply movement vector and speed 
@@ -30,3 +33,42 @@ func _physics_process(delta: float) -> void:
 			hit.get_parent().interact()
 		
 	
+func process_sanity() -> void:
+	if (flashlight.enabled or hasLightSource) and not isAttacked:
+		modify_sanity(SANITY_INCREMENT)
+	else:
+		modify_sanity(-SANITY_INCREMENT)
+	print(sanity)
+	
+	
+func modify_sanity(increment: float) -> void:
+	if sanity + increment > 100:
+		sanity = 100
+	elif sanity + increment < 0:
+		sanity = 0
+	else:
+		sanity += increment
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print(area.collision_layer)
+	if area.collision_layer == 16:
+		hasLightSource = true
+	
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	print(area.collision_layer)
+	if area.collision_layer == 16:
+		hasLightSource = false
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(body.collision_layer)
+	if body.collision_layer == 2:
+		isAttacked = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print(body.collision_layer)
+	if body.collision_layer == 2:
+		isAttacked = false
