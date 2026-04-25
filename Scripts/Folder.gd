dff7cd0abc63fa8992d6fbac59b3681e724e3942
emtu5 <115076076+emtu5@users.noreply.github.com>
2026-04-25 17:19:12 +0300

    folder pause + textures

--- Scripts/Folder.gd
+++ Scripts/Folder.gd
@@ -2,9 +2,11 @@ extends Interactable
 
 enum FolderText {FIRST,SECOND,THIRD,FOURTH,FIFTH}
 @export var selectedText:FolderText
-@onready var textlabel = $CanvasLayer/Panel/Button
+@onready var textlabel = $CanvasLayer/Panel/Label
 @onready var uiNode = $CanvasLayer
 @onready var audio_player = $AudioStreamPlayer2D
+@onready var area = $Area2D/CollisionShape2D
+@onready var sprite = $Sprite2D
 var debug_txt = "res://FolderTexts/LOREM.txt"
 var first_txt = "res://FolderTexts/FirstFile.txt"
 var second_txt = "res://FolderTexts/SecondFile.txt"
@@ -12,6 +14,7 @@ var third_txt = "res://FolderTexts/ThirdFile.txt"
 var fourth_txt = "res://FolderTexts/FourthFile.txt"
 var fifth_txt = "res://FolderTexts/FifthFile.txt"
 var folder_content
+var interacted: bool = false
 
 func _ready():
 	super()
@@ -32,6 +35,7 @@ func interact():
 			textlabel.text = get_fucking_text(fourth_txt)
 		FolderText.FIFTH:
 			textlabel.text = get_fucking_text(fifth_txt)
+	get_tree().paused = true
 	
 	
 func get_fucking_text(filePath):
@@ -42,8 +46,16 @@ func get_fucking_text(filePath):
 
 
 func _on_button_pressed() -> void:
+	if !interacted:
+		interacted = true
+		player.foldercount += 1
+		area.disabled = true
+		sprite.visible = false
 	uiNode.hide()
+	get_tree().paused = false
 	audio_player.play()
 	await audio_player.finished
+	
+	get_tree().paused = false
 	queue_free()
-	player.foldercount += 1
+	
